59a8940802b32b6be78636ba1e0a06e5ed7a015e
romain <romain.willmann@gmail.com>
2016-12-09 01:51:32 +0100

    Corrigés chapitre 3
    Ajout de la correction des deux premeirs exercices du chapitre 3
    Création d'une fichier .gitignore

---
+++ Chapitre 3/Exercices/chapitre3-exercice1/app.js
@@ -0,0 +1,21 @@
+﻿// Exercice 1, chapitre 3 : récupérer deux chiffres envoyés par un formulaire HTML, les soustraire côté serveur puis renvoyer le résultat dans une réponse HTML
+
+//Chargement des modules Express
+var express = require("express");
+
+//Configuration d'Express
+var app = express();
+
+//Envoi du formulaire lorsqu'un utilisateur se connecte sur l'application 
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/formulaire.html");
+});
+
+//Le calcul se fait ici sur la page /calcul
+app.get("/calcul", function(req, res) {
+	res.send(String(Number(req.query.a) - Number(req.query.b)));
+	res.end();
+});
+
+//Démarrage de l'application sur le port 8080
+app.listen(8080);
---
+++ Chapitre 3/Exercices/chapitre3-exercice2/app.js
@@ -0,0 +1,48 @@
+﻿// Exercice 2, chapitre 3 : utiliser un template Swig pour créer trois pages distinctes
+
+//Chargement des modules Express et Swig
+var express = require("express");
+var swig = require("swig");
+
+//Configuration d'Express pour qu'il puisse charger le contenu du dossier img
+var app = express();
+app.use("/img", express.static(__dirname + "/img"));
+
+//Configuration du moteur de rendu
+app.engine("html", swig.renderFile);
+app.set("view engine", "html");
+
+//Ces informations seront insérées dynamiquement par Swig
+var prix = [1.50, 3, 2];
+var titre = ["Pommes", "Bananes", "Ananas"];
+var image = ["/img/pommes.jpg", "/img/bananes.png", "/img/ananas.gif"];
+
+//Création de la première route (/pomme). Lorsque la page est demandée, Swig insère dans le template les premières valeurs des tableaux prix, titre et img
+app.get("/pomme", function(req, res) {
+    res.render(__dirname + "/template.html", {
+        titre: titre[0],
+        source: image[0],
+        prix: prix[0]
+    });
+});
+
+//Création d'une seconde route
+app.get("/banane", function(req, res) {
+    res.render(__dirname + "/template.html", {
+        titre: titre[1],
+        source: image[1],
+        prix: prix[1]
+    });
+});
+
+//Et d'une troisième
+app.get("/ananas", function(req, res) {
+    res.render(__dirname + "/template.html", {
+        titre: titre[2],
+        source: image[2],
+        prix: prix[2]
+    });
+});
+
+//Démarrage de l'application sur le port 8080
+app.listen(8080);
